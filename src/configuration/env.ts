36b468830fc87bf23f77fe6c990b1dd360e0dc2c
Mahdi <mahdipakravan79@gmail.com>
2020-08-24 13:41:46 +0430

    env Crash Fixed

--- src/configuration/env.ts
+++ src/configuration/env.ts
@@ -7,7 +7,7 @@ export default class ConfigEnv{
             case "test":
                 dotenv.config({ path : `.env.test` }); break;
             case "production":
-                dotenv.config({ path : `.env.production` }); brweak;
+                dotenv.config({ path : `.env.production` }); break;
             default :
                 dotenv.config({ path : `.env.development` }); break;
         }
